085c4c6f76d66f44e8892bcdf977b02bde3f8839
Romain Niveau <romain.niveau@gmail.com>
2014-12-31 16:30:56 +0100

    Send mail

--- check-stocks-mail/src/main/java/net/blog/dev/check/stocks/mail/Main.java
+++ check-stocks-mail/src/main/java/net/blog/dev/check/stocks/mail/Main.java
@@ -1,20 +1,24 @@
 package net.blog.dev.check.stocks.mail;
 
 import com.sendgrid.SendGrid;
+import com.sendgrid.SendGridException;
 
 /**
  * Created by Xebia on 30/12/14.
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SendGridException {
         SendGrid sendgrid = new SendGrid(System.getenv("SENDGRID_USERNAME"), System.getenv("SENDGRID_PASSWORD"));
 
         SendGrid.Email email = new SendGrid.Email();
         email.addTo(System.getenv("TEST_MAIL"));
         email.setFrom("other@example.com");
         email.setSubject("Hello World");
-        email.setHtml("<h1>My first email through SendGrid</h1>");
+        email.setHtml("<h1>My first email through SendGrid");
+
+        sendgrid.send(email);
     }
+
 
 }
