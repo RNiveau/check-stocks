e89bd0c8ff3ef9cd4ae33adf48363de6407f9ecf
Romain Niveau <romain.niveau@gmail.com>
2014-12-31 14:39:15 +0100

    Add SendGrid to send email and test

--- check-stocks-mail/src/main/java/net/blog/dev/check/stocks/mail/Main.java
+++ check-stocks-mail/src/main/java/net/blog/dev/check/stocks/mail/Main.java
@@ -1,7 +1,20 @@
 package net.blog.dev.check.stocks.mail;
 
+import com.sendgrid.SendGrid;
+
 /**
  * Created by Xebia on 30/12/14.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        SendGrid sendgrid = new SendGrid(System.getenv("SENDGRID_USERNAME"), System.getenv("SENDGRID_PASSWORD"));
+
+        SendGrid.Email email = new SendGrid.Email();
+        email.addTo(System.getenv("TEST_MAIL"));
+        email.setFrom("other@example.com");
+        email.setSubject("Hello World");
+        email.setHtml("<h1>My first email through SendGrid</h1>");
+    }
+
 }
