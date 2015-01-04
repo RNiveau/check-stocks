14d2c84408f0b267963f0e218511effc730ad581
Romain Niveau <romain.niveau@gmail.com>
2015-01-04 17:25:49 +0100

    Add YahooService to get stocks

---
+++ check-stocks-common/src/main/java/net/blog/dev/services/api/IYahooService.java
@@ -0,0 +1,12 @@
+package net.blog.dev.services.api;
+
+import net.blog.dev.services.domain.YahooResponse;
+
+/**
+ * Created by Xebia on 04/01/15.
+ */
+public interface IYahooService {
+
+    YahooResponse getHistoric(String code, Integer duration);
+
+}
