34ad8cbc8edeac0a485f4e5b0b328e44f0878be9
RomainN <romain.niveau@gmail.com>
2015-01-26 11:56:16 +0100

    Doesn't run saturday and sunday

--- check-stocks-mail/src/main/java/net/blog/dev/check/stocks/mail/controllers/ScanStockController.java
+++ check-stocks-mail/src/main/java/net/blog/dev/check/stocks/mail/controllers/ScanStockController.java
@@ -3,8 +3,12 @@ package net.blog.dev.check.stocks.mail.controllers;
 import net.blog.dev.check.stocks.mail.rules.domain.RuleResult;
 import net.blog.dev.check.stocks.mail.services.api.IMailService;
 import net.blog.dev.check.stocks.mail.services.api.IScanStockService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
 import java.util.List;
 
 /**
@@ -12,7 +16,9 @@ import java.util.List;
  */
 public class ScanStockController {
 
-   private IScanStockService scanStockService;
+    private static Logger logger = LoggerFactory.getLogger(ScanStockController.class);
+
+    private IScanStockService scanStockService;
 
     private IMailService mailService;
 
@@ -23,6 +29,11 @@ public class ScanStockController {
     }
 
     public void execute() {
+        int dayOfWeek = LocalDate.now().get(ChronoField.DAY_OF_WEEK);
+        if (dayOfWeek > 5) {
+            logger.info("DayOfWeek {} > 5, doesn't scan stock", dayOfWeek);
+            return;
+        }
         List<RuleResult> scan = scanStockService.scan();
         mailService.sendMail(scan);
     }
